28e946ab2d7d0527f5eeb406f6c6e20afd7317f2
Tigran Aghinian <taginian@gmail.com>
2024-01-11 23:52:04 +0400

    Adding an addition feature

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const integers = document.querySelectorAll(".integer");
-const displayField = document.querySelector(".display-field");
+const display = document.querySelector(".display");
 const equal = document.querySelector(".equal");
 
 const integersArray = Array.from(integers);
+console.log(integersArray);
 
-let calculation = "";
+let calculation = [];
+let total = "";
 
 integersArray.forEach((integer) => {
   integer.addEventListener("click", () => {
-    displayField.innerHTML = "" + integer.getAttribute("value");
-    calculation += integer.getAttribute("value");
+    display.innerHTML = integer.textContent;
     console.log(calculation);
+    calculation.push(integer.textContent);
+    console.log(calculation);
+    if (calculation.length === 3) {
+      total = Number(calculation[0]) + Number(calculation[2]);
+      calculation = [];
+      calculation[0] = total;
+    }
   });
 });
 
 equal.addEventListener("click", () => {
-  if (calculation[1] === "+") {
-    displayField.innerHTML = Number(calculation[0]) + Number(calculation[2]);
-  } else if (calculation[1] === "-") {
-    displayField.innerHTML = Number(calculation[0]) - Number(calculation[2]);
-  }
+  display.innerHTML = total;
 });
